c02f5875736533c061b6ffa33606894b6149e98f
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-19 16:20:39 -0300

    add: UnitOfWork

---
+++ src/CodigoNaVeia/Infra.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Infra.Data.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        bool Commit();
+    }
+}
---
+++ src/CodigoNaVeia/Infra.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Infra.Data.Context;
+
+namespace Infra.Data.UnitOfWork
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly CodigoNaVeiaContext _context;
+
+        public UnitOfWork(CodigoNaVeiaContext context)
+        {
+            _context = context;
+        }
+
+        public bool Commit()
+        {
+             return _context.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
