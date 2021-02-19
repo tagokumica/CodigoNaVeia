04f34fafec1339ac0b518d4399783df139e95ded
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-19 12:51:43 -0300

    add: Validation Security

---
+++ src/CodigoNaVeia/Infra.CrossCutting.Security/Validation/ForgotPasswordValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Infra.CrossCutting.Security.ViewModel;
+
+namespace Infra.CrossCutting.Security.Validation
+{
+    public class ForgotPasswordValidation : AbstractValidator<ForgotPasswordViewModel>
+    {
+        public ForgotPasswordValidation()
+        {
+            RuleFor(s => s.Email).NotEmpty().WithMessage("Digite o Email").EmailAddress();
+
+
+        }
+    }
+}
---
+++ src/CodigoNaVeia/Infra.CrossCutting.Security/Validation/LoginValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Infra.CrossCutting.Security.ViewModel;
+
+namespace Infra.CrossCutting.Security.Validation
+{
+    public class LoginValidation: AbstractValidator<LoginViewModel>
+    {
+        public LoginValidation()
+        {
+            RuleFor(s => s.Email).NotEmpty().WithMessage("Digite o Email").EmailAddress();
+
+            RuleFor(s => s.Password).NotEmpty().WithMessage("Digite a Senha");
+
+        }
+    }
+}
