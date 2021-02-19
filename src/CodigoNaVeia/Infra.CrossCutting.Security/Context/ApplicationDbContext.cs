9eda309abb80e358f360c448385e6763d28c81e2
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-19 12:50:49 -0300

    add: Contexto BD Identity

---
+++ src/CodigoNaVeia/Infra.CrossCutting.Security/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Infra.CrossCutting.Security.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.CrossCutting.Security.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+
+
+
+
+    }
+}
