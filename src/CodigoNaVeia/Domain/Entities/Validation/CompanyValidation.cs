3951c91caa374aabae0c9729961d2e6b566d60c5
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-19 16:18:17 -0300

    add: Validation Entities

---
+++ src/CodigoNaVeia/Domain/Entities/Validation/CompanyValidation.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Validate;
+using FluentValidation;
+
+namespace Domain.Entities.Validation
+{
+    public class CompanyValidation : AbstractValidator<Company>
+    {
+        public CompanyValidation()
+        {
+            RuleFor(s => CnpjValidate.Validate(s.Cnpj))
+                .Equal(true)
+                .WithMessage("O Cnpj esta invalido");
+
+
+
+            RuleFor(s => IeValidate.Validate(s.Ie))
+                .Equal(true)
+                .WithMessage("O Inscrição Estadual esta invalido");
+
+        }
+    }
+}
---
+++ src/CodigoNaVeia/Domain/Entities/Validation/EmployeeValidation.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Validate;
+using FluentValidation;
+
+namespace Domain.Entities.Validation
+{
+    public class EmployeeValidation : AbstractValidator<Employee>
+    {
+        public EmployeeValidation()
+        {
+            RuleFor(s => CpfValidate.Validate(s.Cpf))
+                .Equal(true)
+                .WithMessage("O Cpf esta invalido");
+
+
+            RuleFor(s => RgValidate.Validate(s.Rg))
+                .Equal(true)
+                .WithMessage("O Rg esta invalido");
+        }
+    }
+}
