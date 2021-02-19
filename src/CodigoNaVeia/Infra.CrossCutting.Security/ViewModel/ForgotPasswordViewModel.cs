d675f573bf1cdbd6d3f0dd2f54bd99f44e5fed7c
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-19 12:52:19 -0300

    add: ViewModel Security

---
+++ src/CodigoNaVeia/Infra.CrossCutting.Security/ViewModel/ForgotPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infra.CrossCutting.Security.ViewModel
+{
+    public class ForgotPasswordViewModel
+    {
+        public string Email { get; set; }
+
+    }
+}
---
+++ src/CodigoNaVeia/Infra.CrossCutting.Security/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infra.CrossCutting.Security.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [Display(Name = "Lembre-Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
