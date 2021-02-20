8478d59e24ac2f0215236eb0332fc36d94ef03d4
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-20 15:08:23 -0300

    add: AutoMapper

---
+++ src/CodigoNaVeia/Application/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using Application.ViewModel;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Address, AddressViewModel>()
+                .ForMember(c => c.CompanyAddressViewModel, b => b.MapFrom(v => v.CompanyAddress))
+                .ReverseMap();
+
+            CreateMap<Company, CompanyViewModel>()
+                .ForMember(c => c.EmployeeViewModel, b => b.MapFrom(v => v.Employee))
+                .ForMember(c => c.CompanyAddressViewModel, b => b.MapFrom(v => v.CompanyAddress))
+                .ReverseMap();
+            CreateMap<CompanyAddress, CompanyAddressViewModel>().ReverseMap();
+            CreateMap<EmployeeViewModel, EmployeeViewModel>().ReverseMap();
+
+
+
+        }
+    }
+}
