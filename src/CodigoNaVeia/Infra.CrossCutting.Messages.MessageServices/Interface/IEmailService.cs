bba2696f8536a0dd5e0225b8644e4baf1ee1ff1e
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-02-27 14:05:09 -0300

    add: EmailService

---
+++ src/CodigoNaVeia/Infra.CrossCutting.Messages.MessageServices/Interface/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Infra.CrossCutting.Messages.MessageServices.Interface
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ src/CodigoNaVeia/Infra.CrossCutting.Messages.MessageServices/Service/EmailService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Infra.CrossCutting.Messages.MessageServices.Interface;
+using Infra.CrossCutting.Messages.MessageServices.Setting.Email;
+using Microsoft.Extensions.Options;
+
+namespace Infra.CrossCutting.Messages.MessageServices.Service
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                Execute(email, subject, message).Wait();
+                return Task.FromResult(0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task Execute(string email, string subject, string message)
+        {
+            try
+            {
+                string toEmail = string.IsNullOrEmpty(email) ? _emailSettings.ToEmail : email;
+
+                MailMessage mail = new MailMessage()
+                {
+                    From = new MailAddress(_emailSettings.UsernameEmail, "")
+                };
+
+                mail.To.Add(new MailAddress(toEmail));
+                mail.CC.Add(new MailAddress(_emailSettings.CcEmail));
+
+                mail.Subject = "Código na Veia " + subject;
+                mail.Body = message;
+                mail.IsBodyHtml = true;
+                mail.Priority = MailPriority.High;
+
+                //outras opções
+                //mail.Attachments.Add(new Attachment(arquivo));
+                //
+
+                using (SmtpClient smtp = new SmtpClient(_emailSettings.PrimaryDomain, _emailSettings.PrimaryPort))
+                {
+                    smtp.Credentials = new NetworkCredential(_emailSettings.UsernameEmail, _emailSettings.UsernamePassword);
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    await smtp.SendMailAsync(mail);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+    }
+}
