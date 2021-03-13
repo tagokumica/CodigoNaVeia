ae4b2f2aae05964b02b18c9d6182b1e48f656145
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-03-13 15:20:30 -0300

    add EmployeeController

---
+++ src/CodigoNaVeia/CodigoNaVeia.UI/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Infra.CrossCutting.Security.Model;
+using Microsoft.AspNetCore.Identity;
+using Application.Interface;
+using Domain.Interface.Notification;
+
+namespace CodigoNaVeia.UI.Controllers
+{
+    public class EmployeeController : BaseController
+    {
+        private readonly IEmployeeAppService _iEmployeeAppService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EmployeeController(INotification iNotification, IEmployeeAppService iEmployeeAppService, UserManager<ApplicationUser> userManager) : base(iNotification)
+        {
+            _iEmployeeAppService = iEmployeeAppService;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
