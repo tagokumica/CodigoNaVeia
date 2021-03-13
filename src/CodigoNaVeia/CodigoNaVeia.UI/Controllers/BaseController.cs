bae3bac2f704429c29d1ab103d6f50a70870654e
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-03-13 16:24:46 -0300

    add BaseController

---
+++ src/CodigoNaVeia/CodigoNaVeia.UI/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interface.Notification;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodigoNaVeia.UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly INotification _iNotification;
+
+
+        public BaseController(INotification iNotification)
+        {
+            _iNotification = iNotification;
+        }
+
+        public bool isValided()
+        {
+            return _iNotification.HaveNotifier();
+        }
+    }
+}
