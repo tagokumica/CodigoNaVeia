daab0a90b5b5fa3397a957832272c17e1e9ead9a
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-03-13 16:26:01 -0300

    fix EmployeeController

--- src/CodigoNaVeia/CodigoNaVeia.UI/Controllers/EmployeeController.cs
+++ src/CodigoNaVeia/CodigoNaVeia.UI/Controllers/EmployeeController.cs
@@ -1,25 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Infra.CrossCutting.Security.Model;
 using Microsoft.AspNetCore.Identity;
 using Application.Interface;
+using Application.ViewModel;
 using Domain.Interface.Notification;
+using Infra.CrossCutting.Messages.MessageServices.Interface;
 
 namespace CodigoNaVeia.UI.Controllers
 {
     public class EmployeeController : BaseController
     {
         private readonly IEmployeeAppService _iEmployeeAppService;
+        private readonly INotification _notification;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public EmployeeController(INotification iNotification, IEmployeeAppService iEmployeeAppService, UserManager<ApplicationUser> userManager) : base(iNotification)
+        public EmployeeController(INotification iNotification, IEmployeeAppService iEmployeeAppService, INotification notification, UserManager<ApplicationUser> userManager) : base(iNotification)
         {
             _iEmployeeAppService = iEmployeeAppService;
+            _notification = notification;
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+
+        [Route("atualizar-empregado/{id:guid}")]
+        public IActionResult Update(Guid id)
         {
-            return View();
+
+            if (id == Guid.NewGuid() || id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var employee = _iEmployeeAppService.FindById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View("Update", employee);
+        }
+
+        [Route("atualizar-empregado/{id:guid}")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(EmployeeViewModel employeeViewModel)
+        {
+
+            var user = _userManager.GetUserAsync(User);
+
+
+            if (ModelState.IsValid)
+            {
+                _iEmployeeAppService.Insert(employeeViewModel);
+                if (isValided())
+                {
+                    ViewBag.Notifications = _notification.Get();
+                    return View(employeeViewModel);
+                }
+                return RedirectToAction("Index", "CompanyEmployee", new { id = user.Result.Id });
+
+            }
+            return View(employeeViewModel);
         }
     }
 }
+
