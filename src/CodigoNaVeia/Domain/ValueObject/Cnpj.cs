e15c5276d23fbd2411d220de9974bfc1ccd03282
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-03-20 14:31:38 -0300

    add Value Object

---
+++ src/CodigoNaVeia/Domain/ValueObject/Cnpj.cs
@@ -0,0 +1,75 @@
+﻿namespace Domain.ValueObject
+{
+    public class Cnpj
+    {
+        public static bool Validate(string cnpj)
+        {
+            int[] multiplier1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplier2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int sum;
+            int rest;
+            string digit;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            if (cnpj.Length != 14)
+                return false;
+
+            switch (cnpj)
+            {
+                case "00000000000000":
+                    return false;
+
+                case "11111111111111":
+                    return false;
+
+                case "22222222222222":
+                    return false;
+
+                case "33333333333333":
+                    return false;
+
+                case "44444444444444":
+                    return false;
+
+                case "55555555555555":
+                    return false;
+
+                case "66666666666666":
+                    return false;
+
+                case "77777777777777":
+                    return false;
+
+                case "88888888888888":
+                    return false;
+
+                case "99999999999999":
+                    return false;
+            }
+
+
+            tempCnpj = cnpj.Substring(0, 12);
+            sum = 0;
+            for (int i = 0; i < 12; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplier1[i];
+            rest = (sum % 11);
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+            digit = rest.ToString();
+            tempCnpj += digit;
+            sum = 0;
+            for (int i = 0; i < 13; i++)
+                sum += int.Parse(tempCnpj[i].ToString()) * multiplier2[i];
+            rest = (sum % 11);
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+            digit += rest.ToString();
+            return cnpj.EndsWith(digit);
+        }
+
+    }
+}
